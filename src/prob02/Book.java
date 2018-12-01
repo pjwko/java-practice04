b632ac3c9ea32c26dd9a8f8811521cd7ceae4d1d
kicks <kickscar@gmail.com>
2018-12-02 07:11:56 +0900

    init commit

---
+++ src/prob02/Book.java
@@ -0,0 +1,49 @@
+package prob02;
+
+public class Book {
+	
+	private int bookNo;
+	private String title;
+	private String author;
+	private int stateCode;
+
+	
+	public int getBookNo() {
+		return bookNo;
+	}
+	
+	public void setBookNo(int bookNo) {
+		this.bookNo = bookNo;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public String getAuthor() {
+		return author;
+	}
+	
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	
+	public int getStateCode() {
+		return stateCode;
+	}
+	
+	public void setStateCode(int stateCode) {
+		this.stateCode = stateCode;
+	}
+	
+	public void rent() {
+
+	}
+	
+	public void print() {
+	}	
+}
